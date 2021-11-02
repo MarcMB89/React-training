9633b609d91a870f7d7771946d3ace26f453ee0d
Marc Martin <marcmartinbarrios@gmail.com>
2021-11-02 18:36:32 +0100

    Edit App - Change rendered components

--- src/App.js
+++ src/App.js
@@ -7,31 +7,29 @@ import Display from "./components/Counter/Display";
 import TimeCounter from "./components/Time/TimeCounter.jsx";
 import HelloHTML from "./components/Time/HelloHTML";
 import GithubApp from "../src/components/GitHubApp/GitHubApp";
-import CardList from "./components/GitHubApp/Card";
 
 function App() {
   const [counter, setCounter] = useState(0);
   const incrementCounter = (incrementValue) => setCounter(counter + incrementValue );
   const decrementCounter = (decrementValue) => setCounter(counter - decrementValue );
-   
+
   return(
 
-  <div className="mountNode">
-      <IncrementButton onClickFunction={incrementCounter} increment={1}/>
-      <IncrementButton onClickFunction={incrementCounter} increment={5}/>
-      <IncrementButton onClickFunction={incrementCounter} increment={10}/>
-      <IncrementButton onClickFunction={incrementCounter} increment={100}/>
-      <DecrementButton onClickFunction={decrementCounter} increment={1}/>
-      <DecrementButton onClickFunction={decrementCounter} increment={5}/>
-      <DecrementButton onClickFunction={decrementCounter} increment={10}/>
-      <DecrementButton onClickFunction={decrementCounter} increment={100}/>
-      <Display message={counter}/>
-      <HelloHTML />
-      <TimeCounter />
-      <GithubApp title="The GitHub Cards App"/>
-      <CardList />
-      
-  </div>
+    <div className="mountNode">
+        <IncrementButton onClickFunction={incrementCounter} increment={1}/>
+        <IncrementButton onClickFunction={incrementCounter} increment={5}/>
+        <IncrementButton onClickFunction={incrementCounter} increment={10}/>
+        <IncrementButton onClickFunction={incrementCounter} increment={100}/>
+        <DecrementButton onClickFunction={decrementCounter} increment={1}/>
+        <DecrementButton onClickFunction={decrementCounter} increment={5}/>
+        <DecrementButton onClickFunction={decrementCounter} increment={10}/>
+        <DecrementButton onClickFunction={decrementCounter} increment={100}/>
+        <Display message={counter}/>
+        <HelloHTML />
+        <TimeCounter />
+        <GithubApp title="The GitHub Cards App"/>
+        
+    </div>
   
   )
 }
