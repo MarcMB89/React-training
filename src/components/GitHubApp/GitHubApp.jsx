54dd588e2968f06dc7c9b4058c7a6e534179ccc2
Marc Martin <marcmartinbarrios@gmail.com>
2021-11-02 18:37:39 +0100

    Edit GitHubApp to convert it to a component

--- src/components/GitHubApp/GitHubApp.jsx
+++ src/components/GitHubApp/GitHubApp.jsx
@@ -1,14 +1,26 @@
 import React from "react";
+import Form from "./Form";
+import CardList from "./Card";
 
 class GitHubApp extends React.Component {
-
-  render() {
-    return (
-      <div className="header">
-        {this.props.title}
-      </div>
+  state = {
+    profiles: [],
+  };
+  addNewProfile = (profileData) => {
+  	this.setState(prevState => ({
+    	profiles: [...prevState.profiles, profileData],
+    }));
+  };
+	render() {
+  	return (
+    	<div>
+    	  <div className="header">{this.props.title}</div>
+        <Form onSubmit={this.addNewProfile} />
+        <CardList profiles={this.state.profiles} />
+    	</div>
     )
-  }
+  }	
 }
+
 
 export default GitHubApp;
