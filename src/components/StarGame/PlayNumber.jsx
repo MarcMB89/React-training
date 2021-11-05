e8bbf6e526954d457fc8b324dd1841594899c081
Marc Martin <marcmartinbarrios@gmail.com>
2021-11-05 16:21:21 +0100

    Add PlayNumber component

---
+++ src/components/StarGame/PlayNumber.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PlayNumber = props => (
+<button 
+    className="number"
+    onClick={() => console.log('Num', props.number)}>
+    {props.number}
+</button>
+)
+
+export default PlayNumber;
