7145977aaaebe24b388f41f0601b7d79a402d35c
Marc Martin <marcmartinbarrios@gmail.com>
2021-11-11 15:30:21 +0100

    Add StarGame logic

--- src/components/StarGame/StarGame.jsx
+++ src/components/StarGame/StarGame.jsx
@@ -1,38 +1,73 @@
 import React, { useState } from "react";
-import DisplayStars from "./DisplayStars";
-
+import StarsDisplay from "./DisplayStars";
 import PlayNumber from "./PlayNumber";
-import utils from "./utils/utils";
+
+import utils from "../../utils/utils";
+import "./StarStyles.css";
 
 const StarMatch = () => {
-  const [stars, setStars] = useState(utils.random(1,9));
-    return (
-      <div className="game">
-        <div className="help">
-          Pick 1 or more numbers that sum to the number of stars
-        </div>
-        <div className="body">
-          <div className="left">
-           <DisplayStars count={stars}/>
-          </div>
-          <div className="right">
-            {utils.range(1, 9).map(number => 
-              <PlayNumber key={number} number={number}/>
-            )}            
-          </div>
-        </div>
-        <div className="timer">Time Remaining: 10</div>
+  const [stars, setStars] = useState(utils.random(1, 9));
+  const [availableNums, setAvailableNums] = useState(utils.range(1, 9));
+  const [candidateNums, setCandidateNums] = useState([]);
+
+  const candidatesAreWrong = utils.sum(candidateNums) > stars;
+
+  const numberStatus = number => {
+    if (!availableNums.includes(number)) {
+      return 'used';
+    }
+    if (candidateNums.includes(number)) {
+      return candidatesAreWrong ? 'wrong' : 'candidate';
+    }
+    return 'available';
+  };
+
+  const onNumberClick = (number, currentStatus) => {
+    if (currentStatus === 'used') {
+      return;
+    }
+
+		const newCandidateNums =
+      currentStatus === 'available'
+        ? candidateNums.concat(number)
+        : candidateNums.filter(cn => cn !== number);
+
+    if (utils.sum(newCandidateNums) !== stars) {
+      setCandidateNums(newCandidateNums);
+    } else {
+      const newAvailableNums = availableNums.filter(
+        n => !newCandidateNums.includes(n)
+      );
+      setStars(utils.randomSumIn(newAvailableNums, 9));
+      setAvailableNums(newAvailableNums);
+      setCandidateNums([]);
+    }
+  };
+
+  return (
+    <div className="game">
+      <div className="help">
+        Pick 1 or more numbers that sum to the number of stars
       </div>
-    );
-  };
-  
-  // eslint-disable-next-line no-unused-vars
-  const colors = {
-    available: 'lightgray',
-    used: 'lightgreen',
-    wrong: 'lightcoral',
-    candidate: 'deepskyblue',
-  };
-  
+      <div className="body">
+        <div className="left">
+          <StarsDisplay count={stars} />
+        </div>
+        <div className="right">
+          {utils.range(1, 9).map(number => (
+            <PlayNumber
+              key={number}
+              status={numberStatus(number)}
+              number={number}
+              onClick={onNumberClick}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="timer">Time Remaining: 10</div>
+    </div>
+  );
+};
+
 export default StarMatch;
   
